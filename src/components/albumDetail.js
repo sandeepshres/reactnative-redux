e200724f734ebd985bc2b53a84948782eb14adca
Sandeep Shrestha <sandeep.c.shrestha@gmail.com>
2019-05-04 08:50:01 -0500

    Reusable components

---
+++ src/components/albumDetail.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './card';
+import CardSection from './cardSection';
+import Button from './button';
+
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image } = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailConainerStyle, headerTextStyle, imageStyle } = styles;
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailConainerStyle}>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    style={imageStyle}
+                    source={{ uri: image }}
+                />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => console.log(title)} />
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailConainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+};
+
+export default AlbumDetail;
