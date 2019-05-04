d87b8651c9b41802987645430521f476a6165b50
Sandeep Shrestha <sandeep.c.shrestha@gmail.com>
2019-05-04 09:10:08 -0500

    setting button text by props

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -28,7 +28,9 @@ const AlbumDetail = ({ album }) => {
                 />
             </CardSection>
             <CardSection>
-                <Button onPress={() => Linking.openURL(url)} />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
